8db667abf9ef54b8507619b5b5735bb34a6b27b2
Paz Manrique <nautilus1985@gmail.com>
2022-10-20 12:49:06 +0200

    showing products from database

---
+++ lacelia/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+ALTER TABLE product AUTO_INCREMENT = 1;
+
+INSERT INTO product (description, name  ) values ("Pollo","genial");
+INSERT INTO product (description, name  ) values ("Huevos","genial");
+INSERT INTO product (description, name  ) values ("Miel","genial");
